8b81b0d1d927520a4b1f90882aecdc92125ada99
mamaevn <mamaevn@edi.su>
2019-05-27 11:10:51 +0300

    Max counters task is optimized.

--- src/main/java/com/codility/MaxCounters04.java
+++ src/main/java/com/codility/MaxCounters04.java
@@ -1,16 +1,22 @@
 package com.codility;
 
-import java.util.Arrays;
-
 public class MaxCounters04 {
     public int[] solution(int N, int[] A) {
         int[] result = new int[N];
         int max = 0;
+        int maxarr = 0;
         for (int counter : A) {
             if (counter <= N) {
                 result[counter - 1]++;
                 if (result[counter - 1] > max) max = result[counter - 1];
-            } else Arrays.fill(result, max);
+            } else {
+                result = new int[N];
+                maxarr += max;
+                max = 0;
+            }
+        }
+        for (int i = 0; i < result.length; i++) {
+            result[i] = result[i] + maxarr;
         }
         return result;
     }
